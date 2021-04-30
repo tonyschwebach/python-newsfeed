1dff424492a86dc4c5b40b304cd23e5f33a78f98
T Schway <tony.schwebach@gmail.com>
2021-04-30 11:16:11 -0400

    Vote model and seeds

--- seeds.py
+++ seeds.py
@@ -36,4 +36,14 @@ db.add_all([
 ])
 db.commit()
 
+# insert votes
+db.add_all([
+  Vote(user_id=1, post_id=2),
+  Vote(user_id=1, post_id=4),
+  Vote(user_id=2, post_id=4),
+  Vote(user_id=3, post_id=4),
+  Vote(user_id=4, post_id=2)
+])
+db.commit()
+
 db.close()
