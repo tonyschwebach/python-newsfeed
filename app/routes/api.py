0237c2a9c6ffc95aa501a2f5be57e53491e64396
T Schway <tony.schwebach@gmail.com>
2021-05-03 18:20:45 -0400

    added api post route for new comments

--- app/routes/api.py
+++ app/routes/api.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, request,jsonify, session
-from app.models import User
+from app.models import User, Post, Comment, Vote
 from app.db import get_db
 import sys
 
@@ -53,3 +53,22 @@ def login():
   session['user_id'] = user.id
   session['loggedIn'] = True
   return jsonify(id = user.id)
+
+@bp.route('/comments',methods=['POST'])
+def comment():
+  data = request.get_json()
+  db = get_db()
+
+  try:
+    newComment = Comment(
+      comment_text = data['comment_text'],
+      post_id = data['post_id'],
+      user_id = session.get('user_id'))
+    db.add(newComment)
+    db.commit()
+  except:
+    print(sys.exc_info()[0])
+    db.rollback()
+    return jsonify(message='Comment failed'),500
+  
+  return jsonify(id = newComment.id)
